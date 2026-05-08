5e2fb3def2870a3300a4edee1d4f76963728e8b9
manomelo <lucas.melo2103@gmail.com>
2026-05-08 13:39:25 -0300

    feat(produtos): CRUD completo com views, templates, forms e admin

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Produto
+from .forms import ProdutoForm
 
-# Create your views here.
+
+def listar_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'produtos/listar.html', {'produtos': produtos})
+
+
+def criar_produto(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_produtos')
+    else:
+        form = ProdutoForm()
+    return render(request, 'produtos/form.html', {'form': form, 'titulo': 'Novo Produto'})
+
+
+def editar_produto(request, pk):
+    produto = get_object_or_404(Produto, pk=pk)
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_produtos')
+    else:
+        form = ProdutoForm(instance=produto)
+    return render(request, 'produtos/form.html', {'form': form, 'titulo': 'Editar Produto'})
+
+
+def excluir_produto(request, pk):
+    produto = get_object_or_404(Produto, pk=pk)
+    if request.method == 'POST':
+        produto.delete()
+        return redirect('listar_produtos')
+    return render(request, 'produtos/confirmar_exclusao.html', {'produto': produto})
